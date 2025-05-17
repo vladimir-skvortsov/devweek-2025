82b22c9ff4f0b9d91afa71e0a59f9001be04b7fb
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2025-05-17 14:09:59 +0300

    feat: add langgraph

---
+++ model/model.py
@@ -0,0 +1,28 @@
+from typing import Annotated
+
+from typing_extensions import TypedDict
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
+
+def chatbot(state: State):
+    return {"messages": [*state["messages"], "Hello, I'm fine, thanks"]}
+
+
+graph_builder.add_node("chatbot", chatbot)
+
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile()
+
+
+print(graph.invoke({"messages": ["Hello, how are you?"]}))
