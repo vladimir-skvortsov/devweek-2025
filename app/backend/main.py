38125fcfa029f348fc0ddd09af5b4224caea00e6
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2025-05-18 02:57:53 +0300

    feat: use model in api

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,19 +1,29 @@
+import sys
+
+sys.path.append('../..')
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 
 from config import PROJECT_NAME
+from model.model import Model
+
+load_dotenv()
 
 app = FastAPI(title=PROJECT_NAME)
+model = Model()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 
-@app.get("/")
-async def root():
-    return {"message": f"Welcome to {PROJECT_NAME}!"}
+@app.get('/api/v1/score')
+async def root(text: str):
+    score = await model.ainvoke({'text': text})
+    return {'score': score}
