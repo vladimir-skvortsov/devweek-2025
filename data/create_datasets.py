284f4e56a5c46a7d9a5236fee2ad635682637713
afafos <afanasyev239@gmail.com>
2025-05-18 19:36:12 +0300

    kaggle dataset through api

--- data/create_datasets.py
+++ data/create_datasets.py
@@ -1,3 +1,7 @@
+import os
+from dotenv import load_dotenv
+import kagglehub
+import pandas as pd
 import uuid
 import pathlib
 from datasets import load_dataset, concatenate_datasets, Value, Dataset
@@ -10,20 +14,38 @@ def load_hf(repo: str) -> Dataset:
     return ds.remove_columns([c for c in ds.column_names if c not in keep])
 
 
+def load_kaggle_as_hf() -> Dataset:
+    load_dotenv()
+    if os.getenv("KAGGLE_USERNAME") and os.getenv("KAGGLE_KEY"):
+        os.environ["KAGGLE_USERNAME"] = os.getenv("KAGGLE_USERNAME")
+        os.environ["KAGGLE_KEY"] = os.getenv("KAGGLE_KEY")
+
+    path = kagglehub.dataset_download("sunilthite/llm-detect-ai-generated-text-dataset")
+    print("Path to dataset files:", path)
+    files = os.listdir(path)
+    print("Files in dataset folder:", files)
+    csv_files = [f for f in files if f.endswith('.csv')]
+    if not csv_files:
+        raise Exception("There are no csv files in the folder")
+    kaggle_df = pd.read_csv(os.path.join(path, csv_files[0]))
+
+    if 'text' not in kaggle_df.columns or 'generated' not in kaggle_df.columns:
+        raise Exception(f"Fields were expected text and generated, but actually: {kaggle_df.columns}")
+
+    kaggle_df = kaggle_df[['text', 'generated']].copy()
+    kaggle_df['is_human'] = 1 - kaggle_df['generated']
+    kaggle_df['id'] = [uuid.uuid4().hex for _ in range(len(kaggle_df))]
+    kaggle_df = kaggle_df[['id', 'text', 'is_human']]
+
+    ds_kaggle = Dataset.from_pandas(kaggle_df, preserve_index=False)
+    return ds_kaggle
+
 # Грузим HF
 ds_hf1 = load_hf('shahxeebhassan/human_vs_ai_sentences')
 ds_hf2 = load_hf('ardavey/human-ai-generated-text')
 
 # Грузим Kaggle
-csv_path = pathlib.Path(__file__).with_name('training_essay_data.csv')
-ds_csv = (
-    load_dataset('csv', data_files=str(csv_path), split='train')
-    .cast_column('is_human', Value('int64'))
-    .map(lambda _: {'id': uuid.uuid4().hex})
-)
-
-keep = {'id', 'text', 'is_human'}
-ds_csv = ds_csv.remove_columns([c for c in ds_csv.column_names if c not in keep])
+ds_csv = load_kaggle_as_hf()
 
 # Full dataset
 full_ds = concatenate_datasets([ds_hf1, ds_hf2, ds_csv])
